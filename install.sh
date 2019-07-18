e92cf9d6f64dfa53bc8bf3991c89046d93e9b08a
Justin Keenan <JustinJKeenan@gmail.com>
2019-07-18 11:19:29 -0400

    updated install for installing wget and homebrew:

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 pip3 install -r requirements.txt
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
 wget https://archive.scryfall.com/json/scryfall-default-cards.json
 brew install sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
 cd dependencies
